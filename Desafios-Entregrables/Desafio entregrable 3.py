a0194fcdf74d016381b18f3861d8c7a33f645dd0
Luis Alfonzo <patitoloco030@gmail.com>
2023-05-09 18:57:51 -0300

    Desafio-Semana-3

--- Desafios-Entregrables/Desafio entregrable 3.py
+++ Desafios-Entregrables/Desafio entregrable 3.py
@@ -20,3 +20,64 @@
 #* Si el usuario no ingresa un número entero no debes descontarle un intento, en los demás casos si debes descontarle un intento.
 #* En cada intento debes informarle al usuario los intentos que le quedan disponibles y solicitarle que ingrese otro número.
 #* Si el usuario no acierta en los 8 intentos, debes informarle que se agotaron los intentos y cuál era el número que tenía que adivinar.
+
+# Inportar la biblioteca random para generar un numero random con random.randint
+
+import random
+
+# Pedimos al usuario que ingrese su nombre
+
+nombre = input("¡Bienvenido! ¿Cómo te llamas? ")
+
+# Generamos un número aleatorio entre 1 y 100
+
+numero_a_adivinar = random.randint(1, 100)
+
+# Inicializamos el número de intentos en 8
+
+intentos_restantes = 8
+
+# Comenzamos el juego
+
+print(f"{nombre}, estoy pensando en un número entre 1 y 100. Tienes 8 intentos para adivinarlo.")
+
+# Mientras queden intentos, seguimos jugando
+
+while intentos_restantes > 0:
+
+    # Pedimos al usuario que ingrese un número
+
+    numero_str = input("Ingresa un número: ")
+    
+    # Verificamos que el usuario haya ingresado un número entero
+
+    if not numero_str.isdigit():
+        print("Eso no es un número entero. Intenta de nuevo.")
+        continue
+    
+    # Convertimos el número ingresado a entero
+
+    numero_usuario = int(numero_str)
+    
+    # Descontamos un intento al usuario
+
+    intentos_restantes -= 1
+    
+    # Verificamos si el usuario adivinó el número
+
+    if numero_usuario == numero_a_adivinar:
+        print(f"¡Felicidades, {nombre}! Adivinaste el número en el intento {9 - intentos_restantes}.")
+        break
+    elif numero_usuario < numero_a_adivinar:
+        print("El número a adivinar es mayor.")
+    else:
+        print("El número a adivinar es menor.")
+    
+    # Informamos al usuario cuántos intentos le quedan
+
+    print(f"Te quedan {intentos_restantes} intentos.")
+    
+# Si se acabaron los intentos y el usuario no adivinó el número, se lo informamos
+
+if intentos_restantes == 0:
+    print(f"Lo siento, {nombre}, se te acabaron los intentos. El número que debías adivinar era {numero_a_adivinar}.")
