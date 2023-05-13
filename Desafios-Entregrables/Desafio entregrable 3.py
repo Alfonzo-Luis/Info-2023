31b343840c883099a16ef4118f35fb93d81a8cd4
Luis Alfonzo <patitoloco030@gmail.com>
2023-05-12 21:24:31 -0300

    Limpio

--- Desafios-Entregrables/Desafio entregrable 3.py
+++ Desafios-Entregrables/Desafio entregrable 3.py
@@ -1,26 +1,3 @@
-#Desafío 3: Adivina el número
-#Requisitos técnicos:
-#- Operadores lógicos.
-#- Operadores de comparación.
-#- Control de flujo - Condicionales.
-#- Control de Flujo - Repetitivas.
-#Vamos a crear un juego completamente funcional.
-#Para ello el programa debe:
-#* Pedir al usuario que ingrese su nombre.
-#* Informarle que el número a adivinar está entre 1 y 100, y que tiene 8 intentos para adivinarlo.
-#* Generar aleatoriamente un número entero entre 1 y 100.
-#tip 1: importar de la biblioteca random la función randint (Tu profe te explicará en clase cómo hacerlo)
-#* Informarle al usuario cuántos intentos le quedan y solicitarle que ingrese un número.
-#El "número" ingresado por el usuario puede:
-#* No ser un número entero, en éste caso avisarle que no es un número entero el que ingresó.
-#tip 2: con el método isdigit() puedes saber si es posible convertir a entero
-#* Ser menor al que tiene que adivinar, en éste caso informarle que el número a adivinar es mayor.
-#* Ser mayor al que tiene que adivinar, en éste caso informarle que el número a adivinar es menor.
-#* Igual al que tiene que adivinar, en éste caso informarle que ha ganado y decirle en cuál intento lo adivinó.
-#* Si el usuario no ingresa un número entero no debes descontarle un intento, en los demás casos si debes descontarle un intento.
-#* En cada intento debes informarle al usuario los intentos que le quedan disponibles y solicitarle que ingrese otro número.
-#* Si el usuario no acierta en los 8 intentos, debes informarle que se agotaron los intentos y cuál era el número que tenía que adivinar.
-
 # Inportar la biblioteca random para generar un numero random con random.randint
 
 import random
@@ -89,4 +66,14 @@ while intentos_restantes > 0:
 if intentos_restantes == 0:
     print(f"Lo siento, {nombre}, se te acabaron los intentos. El número que debías adivinar era {numero_a_adivinar}.")
 
-#Integrantes G6: Lucas Matias Almiron, Mauricio Acevedo, Gustavo Medina, Alfonzo Luis Andres, Omar Alejandro Maciel, Alejandor Gutierrez, Federico Peralta, Matias Martin Satina, Rodolfo Sanchez, Luis Marcos Antonio Leiva
+#Integrantes G6:
+# Lucas Matias Almiron
+# Mauricio Acevedo
+# Gustavo Medina
+# Alfonzo Luis Andres
+# Omar Alejandro Maciel
+# Alejandor Gutierrez
+# Federico Peralta
+# Matias Martin Satina
+# Rodolfo Sanchez
+# Luis Marcos Antonio Leiva
